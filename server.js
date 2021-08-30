37266b5e9239fc122c25049d31f1bcf6add6534b
Mikhail <zemiza@yandex.ru>
2021-08-30 16:40:05 +0300

    first commit

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ app.use(express.static(path.resolve(__dirname, 'dist')))
 
 
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'build', 'index.html'))
+    res.sendFile(path.join(__dirname, 'dist', 'index.html'))
 })
 
 const port = process.env.PORT || 8080;
